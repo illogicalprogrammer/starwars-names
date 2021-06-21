cf00967d7254605cd911b5713436bcc60c4c7943
Ankit Kumar <ankitkumar@Ankitkumar-MBP.local>
2021-06-21 11:53:04 +0530

    Library with import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-var starWarsNames = require('./starwars-names.json');
-var randomStarWarsName = require('unique-random-array');
+// import uniqueRandomArray from 'unique-random-array';
+// import starWarsNames from './starwars-names.json';
 
-module.exports = {
-    all : starWarsNames,
-    random : randomStarWarsName(starWarsNames)
-}
+// const all = starWarsNames;
+// const random = uniqueRandomArray(starWarsNames);
+
+// export {all, random};
+import randomArray from 'unique-random-array';
+import starWarsNames from './starwars-names.json';
+
+export const all = starWarsNames;
+export const random = randomArray(starWarsNames);
+
