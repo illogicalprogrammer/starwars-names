632a2af42cb6cba0bb1fa42d57ff783ab7f973c2
Ankit Kumar <ankitkumar@Ankitkumar-MBP.local>
2021-06-21 12:21:45 +0530

    Using v1.0.0 of unique-random-array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-// import uniqueRandomArray from 'unique-random-array';
-// import starWarsNames from './starwars-names.json';
+var uniqueRandomArray = require('unique-random-array');
+var starWarsNames = require('./starwars-names.json');
 
-// const all = starWarsNames;
-// const random = uniqueRandomArray(starWarsNames);
-
-// export {all, random};
-import randomArray from 'unique-random-array';
-import starWarsNames from './starwars-names.json';
-
-export const all = starWarsNames;
-export const random = randomArray(starWarsNames);
-
+module.exports = {
+    all : starWarsNames,
+    random : uniqueRandomArray(starWarsNames)
+};
